59a6b9a27d6f60a0a9002f598fb8c14f031965d9
Emma Gifford <emmagifford@u.boisestate.edu>
2024-09-21 13:32:37 -0600

    add user input, rough draft

--- app/main.c
+++ app/main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+
 #include "../src/lab.h"
 
 int main(int argc, char * argv[])
 {
   printf("hello world\n");
 
+  char *line;
+
   parse_args(argc, argv);
 
   // initialize a new shell :)
+  // sh_init();
+
+  using_history();
+  while((line = readline("$"))) {
+    printf("%s\n", line);
+    add_history(line);
+    free(line);
+  }
+
+  // sh_destroy();
 
   return 0;
 }
